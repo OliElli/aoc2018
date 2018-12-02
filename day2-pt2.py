85076ffb0a6c69ece4686748132fe7333a594b39
Oliver Elliott <oliver.elliott@gmail.com>
2018-12-02 21:31:49 +0000

    first successful attempt

---
+++ day2-pt2.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+input = open("day2-input.txt", "r")
+# input = open("test-input.txt", "r")
+lines = input.readlines()
+i = 0
+match = 0
+
+for line in lines:
+    line = line.rstrip()
+    i += 1
+    for line2 in lines[i:]:
+        output = ""
+        match = 0
+        letter_num = 0
+        for letter in line:
+            if letter == line2[letter_num]:
+                match += 1
+                output += letter
+            letter_num += 1
+        if len(line) - match == 1:
+            print(line)
+            print(line2)
+            print("Output: " + output)
