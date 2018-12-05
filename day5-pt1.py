7a59ea939a276a21dc180e204c509d4504da42c9
Oliver Elliott <oliver.elliott@gmail.com>
2018-12-05 22:11:53 +0000

    Faster solution

--- day5-pt1.py
+++ day5-pt1.py
@@ -11,17 +11,42 @@ lines = input.readlines()
 def find_dupes(line): 
   i = 0
   char = ""
-  output = ""
+  # output = ""
+  dupe = 0
+  positions = []
   for x in line:
-    string = line
+    if dupe == 1:
+      dupe = 0
+      continue
     if char.lower() == x.lower():
       if (char.islower() and x.isupper()) or (char.isupper() and x.islower()):
-        output = str(string[:i-1] + string[i+1:])
-        string = find_dupes(output)
-        return(string)
+        positions.append(i)
+        if i < len(line) - 1:
+          char = line[i+1]
+        else:
+          char = x
+        i += 2
+        dupe = 1
+        continue
     char = x
     i += 1
-  return(string)
+  
+  # Remove the duplicates in one go
+  j = 0
+  for i in positions:
+    line = str(line[:i-1-j] + line[i+1-j:])
+    j += 2
 
-result = find_dupes(lines[0])
-print(len(result))
+  return(line)
+
+string = lines[0]
+
+# Iterate until joined duplicates are taken care of
+while True:
+  string_before = string
+  string = find_dupes(string)
+  if len(string_before) == len(string):
+    break
+
+print(len(string))
+
